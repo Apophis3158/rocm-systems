5ca06661a910894fe52c257ce460563769eaa8fb
Apophis <apophis3158@gmail.com>
2026-05-16 09:48:14 +0800

    support sccache

--- .apophis/build_clr.ps1
+++ .apophis/build_clr.ps1
@@ -4,9 +4,10 @@ $ErrorActionPreference = "Stop"
 $VenvPath = "H:\ROCm\.venv"
 
 . "$VenvPath\Scripts\activate.ps1"
-$ROCM_ROOT = (rocm-sdk path --root).Replace('\', '/')
-$env:ROCM_PATH = $ROCM_ROOT
-$env:PATH = "$ROCM_ROOT/bin;$env:PATH"
+$ROCmBin = (rocm-sdk path --bin).Replace('\', '/')
+$env:PATH = "$ROCmBin;$env:PATH"
+$ROCmRoot = (rocm-sdk path --root).Replace('\', '/')
+$env:ROCM_PATH = $ROCmRoot
 
 $VsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath
 . "$VsPath\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64
@@ -18,9 +19,22 @@ if (Test-Path $BuildDir) { Remove-Item -Recurse -Force $BuildDir }
 # $InstallDir = "$BuildDir/install"
 $InstallDir = "$VenvPath\Lib\site-packages\_rocm_sdk_core"
 
-$env:HIP_CLANG_PATH = "$ROCM_ROOT/lib/llvm"
-$env:HIP_DEVICE_LIB_PATH = "$ROCM_ROOT/lib/llvm/amdgcn/bitcode"
+$LLVMPath = "$ROCmRoot/lib/llvm"
+$env:HIP_CLANG_PATH = "$LLVMPath/bin"
+$env:HIP_DEVICE_LIB_PATH = "$LLVMPath/amdgcn/bitcode"
 $env:CMAKE_BUILD_PARALLEL_LEVEL = "12"
+
+if (Get-Command sccache -ErrorAction SilentlyContinue) {
+    $env:CMAKE_C_COMPILER_LAUNCHER = "sccache"
+    $env:CMAKE_CXX_COMPILER_LAUNCHER = "sccache"
+    $SccacheArgs = @(
+        "-DCMAKE_C_COMPILER_LAUNCHER=sccache",
+        "-DCMAKE_CXX_COMPILER_LAUNCHER=sccache"
+    )
+    Write-Host -ForegroundColor Cyan "* sccache detected, using as compiler launcher"
+} else {
+    $SccacheArgs = @()
+}
 
 cmake -GNinja `
     -S $SuperRoot/projects/clr `
@@ -30,7 +44,7 @@ cmake -GNinja `
     -DHIP_PLATFORM=amd `
     -DCLR_BUILD_HIP=ON `
     -DHIP_COMMON_DIR="$SuperRoot/projects/hip" `
-    -DHIPCC_BIN_DIR="$ROCM_ROOT/bin" `
+    -DHIPCC_BIN_DIR="$ROCmBin" `
     -D__HIP_ENABLE_PCH=OFF `
     -DROCCLR_ENABLE_HSA=ON `
     -DROCCLR_ENABLE_PAL=ON `
@@ -38,11 +52,12 @@ cmake -GNinja `
     -DROCM_KPACK_ENABLED=ON `
     -DAMD_COMPUTE_WIN="$SuperRoot/shared/amdgpu-windows-interop" `
     -DCOMGR_DLL_NAME="amd_comgr0713.dll" `
-    -DClang_ROOT="$ROCM_ROOT/lib/llvm" `
-    -DLLVM_BIN="$ROCM_ROOT/lib/llvm/bin" `
+    -DClang_ROOT="$LLVMPath" `
+    -DLLVM_BIN="$env:HIP_CLANG_PATH" `
     -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded `
-    -Wno-dev
-if ($LASTEXITCODE -ne 0) { $env:PATH -split ';'; Write-Host -ForegroundColor Red "Failed to CMake"; exit $LASTEXITCODE }
-Write-Host -ForegroundColor Green "** CMake configuration done"
+    -Wno-dev `
+    @SccacheArgs
+if ($LASTEXITCODE -ne 0) { $env:PATH -split ';'; Write-Host -ForegroundColor Red "* Failed to CMake"; exit $LASTEXITCODE }
+Write-Host -ForegroundColor Green "* CMake configuration done"
 
 cmake --build $BuildDir -j $env:CMAKE_BUILD_PARALLEL_LEVEL --target install
